26145e7999215ebd0fda9bfa4d96f6812858f0ec
Brokeice123 <keithmarkomen@gmail.com>
2024-06-18 11:35:53 +0300

    Trial

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,18 @@
+// Display the welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Handle user input
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+    // Trim the name and display the message
+    process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
+    process.stdin.end(); // End the stdin stream
   }
 });
 
+// Handle end of input
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
