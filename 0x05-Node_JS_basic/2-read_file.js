99d58721e8aa655cefcb0d3b61b782807d95e052
Brokeice123 <keithmarkomen@gmail.com>
2024-06-18 11:54:14 +0300

    Trial

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,40 @@
+// Import 'fs' module for handling the file system
 const fs = require('fs');
 
+// Define function taking path argument that specify path to database file
 function countStudents(path) {
   try {
+    // Read the database file synchronously
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
-    const students = lines.slice(1); // Skip the header line
-
-    if (students.length === 0) {
-      throw new Error('Cannot load the database');
-    }
-
-    console.log(`Number of students: ${students.length}`);
-
-    const fields = {};
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(','); // Ignore lastname and age
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstname);
+    /* Parse the data and split it using '\n' and ',' as delimeters
+    Store the result in an array */
+    const result = [];
+    data.split('\n').forEach((data) => {
+      result.push(data.split(','));
     });
 
-    for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      }
-    }
-  } catch (err) {
-    throw new Error('Cannot load the database');
+    // Extract and filter needed info and add 1st and 4th fields to array newFields
+    result.shift();
+    const newFields = [];
+    result.forEach((data) => {
+      newFields.push([data[0], data[3]]);
+    });
+
+    // Count, aggregate the data and create a new set
+    const fields = new Set();
+    newFields.forEach((item) => {
+      fields.add(item[1]);
+    });
+    const final = {};
+    fields.forEach((data) => { (final[data] = 0); });
+    newFields.forEach((data) => { (final[data[1]] += 1); });
+    // Display the results on the standard output
+    console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+    Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newFields.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+
+    // Handle the error if the database is not present.
+  } catch (E) {
+    throw Error('Cannot load the database');
   }
 }
 
