568ff49add43f655e3b4879532731a1b3a225b19
Brokeice123 <keithmarkomen@gmail.com>
2024-06-19 19:27:14 +0000

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleLogSpy;
+
+  beforeEach(function() {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    consoleLogSpy.restore();
+  });
+
+  it('should log the correct total for 100 and 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log the correct total for 10 and 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWithExactly('The total is: 20')).to.be.true;
+  });
+});
+
