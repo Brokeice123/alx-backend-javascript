53dafe423d37f5935c40909ff5169aa859195d2a
Brokeice123 <keithmarkomen@gmail.com>
2024-06-19 19:31:43 +0000

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise with the correct object when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then(response => {
+        expect(response).to.be.an('object');
+        expect(response).to.have.property('data', 'Successful response from the API');
+        done();
+      })
+      .catch(err => done(err));
+  });
+});
+
